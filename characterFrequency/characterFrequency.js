dfffb7b20c2d7effe37630380de9564226d58def
Eric Park <erichoonpark@gmail.com>
2018-05-25 11:49:45 -0700

    First draft of charFrequency

--- characterFrequency/characterFrequency.js
+++ characterFrequency/characterFrequency.js
@@ -36,6 +36,39 @@
  */
 
 
-var characterFrequency = function(string) {
+const characterFrequency = function(string) {
+  // Summary: Take a string and return a multi dimensional array that states the number of each char in the string
+  // Input: String
+  // Output: Multidimensional array that displays the frequency of each character
+  // Constraints: The order must be alphabetical
+  // Edge Cases: Capitals, special chars, multiple words
+
+  // Declare an array for the result (result)
+  let result = [];
+  // Declare an object for the char (charObj)
+  let charObj = {};
+  // Convert the string into an array
+  string = string.split('');
+  // Loop through each character in the inputArr
+  for (let i = 0; i < string.length; i++) {
+    // if char doesn't exist in obj, create a new property with a value one
+    if (!(charObj.hasOwnProperty(string[i]))) {
+      charObj[string[i]] = 1;
+    } else {
+      // else increment the value by one
+      charObj[string[i]] += 1;
+    }
+  }
+
+  let allKeys = Object.keys(charObj);
+  // Find all the keys of the object using Object.keys(charObj)
+  // Store keys into a new var (keys)
+  // Sort the keys within the keys array for alphabetical order
+  allKeys = allKeys.sort()
+  // Using a for loop or forEach, push each key and its corresponding value into the returnArr
+  allKeys.forEach(function(element) {
+    result[element] = charObj[element];
+  })
+  //
   return result;
 };
