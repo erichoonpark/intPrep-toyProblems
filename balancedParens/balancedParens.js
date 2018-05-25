4d1e752e2ecb5b8856216673fa6b7f6d49b69fd3
Eric Park <erichoonpark@gmail.com>
2018-05-25 14:27:55 -0700

    First attempt balancedParens

--- balancedParens/balancedParens.js
+++ balancedParens/balancedParens.js
@@ -24,6 +24,29 @@
  *
  */
 var balancedParens = function(input) {
+  // Summary: Check if an input has a balanced number and frequency of parentheses
+  // Input: String
+  // Output: boolean that determines if the parens are balanced
+  // Constraints: Balanced parens means multiple things, some characteristics are:
+    // Parens are [],{},()
+    // Parens enclose one another
+    // Parens can be balanced at center or individual
+  // Edge Cases:
+
+  // Must be an even number
+  // Every parens must have a corresponding closing parens
+  // Must be able to keep track index of parens and closing parens
+  // Declare a variable with all parens
+  let parens = ['(',')','[',']','{','}'];
+  // Declare an obj to keep track of all parens and their location
+  let parensObj = {};
+  // Use a for loop to get through char of the input
+   // If the char is a parens, store it into obj as an array with its location
+   for(let i = 0; i < input.length; i++) {
+     let character = input.charAt(i);
+     if(parens.indexOf(character) !== -1) {
+       parensObj[character] = i;
+     }
+   }
+   return parensObj;
 };
-
-
